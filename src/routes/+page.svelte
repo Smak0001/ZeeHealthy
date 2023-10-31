4e4c5825863804a29cb90e0acfb58d55e20b00ad
getouttar <getouttar@outlook.com>
2023-10-31 22:50:48 +0100

    Added skeleton about page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,56 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Agbalumo&display=swap" rel="stylesheet">
+<div class="header-container"><h1 class="header">ZEEHEALTHY</h1></div>
+<div class="content-container"><div class="content"><div class="para para-1">“Processed foods, such as ready meals, baked goods, and processed meats, can have negative health effects. Consuming highly or heavily processed foods can lead to weight gain, increase the risk of type 2 diabetes, and more.”</div>
+<br>
+<div class="para para-2">These risks should be taken seriously, and healthy eating habits should be built to protect your physical wellbeing. We know that our daily lives can be very stressful, and due to the pressure we feel, we do not have the time to think about what we eat and often end up with fast food.</div>
+<br>
+<div class="para para-3">We, the developers of Zeehealthy, got your back and found a way to provide you with healthy and local food for a fair price.
+The app Zeehealthy aims to support people who want to eat fresh and healthy by providing a system that solves the issues of time and transportation to directly visit local farmers. In the application, the offers from local farmers can be seen, and an order can be placed, which will be picked up for you, by one of the drivers from your neighborhood.</div></div></div>
+
+<style>
+	.header-container {
+		background: blue;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		border-radius: 15px;
+		margin-bottom: 20px;
+	}
+
+	.header {
+		color: #fff;
+		font-family: Tahoma;
+	}
+
+	.content {
+		display: flex;
+		flex-direction: column;
+	}
+
+	.para {
+		width: 50%;
+		font-size: 2em;
+		background: rgba(127, 157, 252, 0.7);
+		padding: 30px;
+		color: #fff;
+		font-family: 'Agbalumo';
+		margin-bottom: 30px;
+		border-radius: 50px;
+	}
+
+	.para-1 {
+		margin-left: 80px;
+	}
+
+	.para-2 {
+		align-self: flex-end;
+		margin-right: 80px;
+	}
+
+	.para-3 {
+		width: 70%;
+		margin-left: 80px;
+	}
+</style>
