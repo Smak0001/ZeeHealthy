964541a72263bdef7def8e36a3b3ef9af354f2fb
Ivan Karev <ivankarev1122@gmail.com>
2023-11-22 12:48:47 +0100

    Register page fixed

--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -17,8 +17,8 @@
       <h1>Sign Up</h1>
       <p>Please fill in this form to create an account.</p>
       <hr>
-      <label for="email"><b>Email</b></label>
-      <input type="text" placeholder="Enter Email" name="email" required>
+      <label for="name"><b>Name</b></label>
+      <input type="text" placeholder="Enter name" name="name" required>
 
       <label for="psw"><b>Password</b></label>
       <input type="password" placeholder="Enter Password" name="psw" required>
