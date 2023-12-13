bec401e60a6c363debe4a32e8aec8b288343b1f7
Emil Hristov <giannirodar@gmail.com>
2023-12-13 14:40:41 +0100

    Deleting users from db

--- svelte.config.js
+++ svelte.config.js
@@ -7,7 +7,12 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter()
-	}
+	},
+	files: {
+		// Explicitly set the entry point
+		template: 'src/routes/index.html',
+	  },
+	  
 };
 
 export default config;
