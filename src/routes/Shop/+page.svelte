9fb302c6edd753534488645a54ec48b689629fec
tygovanderlinde2 <Lind0133@hz.nl>
2023-12-17 22:30:03 +0100

    product -> product.name in button

--- src/routes/Shop/+page.svelte
+++ src/routes/Shop/+page.svelte
@@ -118,7 +118,7 @@
       <p>
         <button
           class="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-          on:click={() => addToCart(product, 1)}
+          on:click={() => addToCart(product.name, 1)}
         >
           Add to Cart
         </button>
