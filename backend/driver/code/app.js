40f057c33e2364a9da7d2b95acfee673f5369d21
getouttar <getouttar@outlook.com>
2023-12-18 12:23:46 +0100

    Change the port

--- backend/driver/code/app.js
+++ backend/driver/code/app.js
@@ -4,14 +4,13 @@ import driversRouter from './routes/drivers.js';
 import ordersRouter from './routes/orders.js';
 
 const app = express();
+const PORT = 3003;
 
 app.use(express.json())
   .use(express.urlencoded({ extended: false }));
 
 app.use('/drivers', driversRouter);
 app.use('/orders', ordersRouter);
-
-const PORT = 3030;
 
 export default app;
 
