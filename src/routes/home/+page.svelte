627a36aad9998f7e82dbeef8578fa958ad5184f7
Emil Hristov <giannirodar@gmail.com>
2024-01-17 11:08:43 +0100

    access admin page

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -34,7 +34,8 @@
 	let { supabase, session } = data;
 	$: ({ supabase, session } = data);
 </script>
-
+{#if session?.user.email === 'admin@admin.com' }
+	
 <div
 	class="header-container bg-blue-500 text-white py-4 flex justify-between items-center"
 >
@@ -43,7 +44,7 @@
 	</div>
 	<nav class="flex justify-end items-center mr-4">
 		<a href="/" class="text-white hover:text-gray-300 mr-4">Home</a>
-		<a href="/admin" class="text-white hover:text-gray-300 mr-4">Admin</a>
+		<a href="/Admin" class="text-white hover:text-gray-300 mr-4">Admin</a>
 		<a href="/shop" class="text-white hover:text-gray-300 mr-4">Shop</a>
 		<a href="/driver" class="text-white hover:text-gray-300 mr-4">Driver</a>
 		<a href="/farmer" class="text-white hover:text-gray-300 mr-4">Farmer</a>
@@ -56,6 +57,28 @@
 		>
 	</nav>
 </div>
+{:else}
+<div
+	class="header-container bg-blue-500 text-white py-4 flex justify-between items-center"
+>
+	<div class="ml-4">
+		<a href="/home" class="text-4xl font-bold">ZeeHealthy</a>
+	</div>
+	<nav class="flex justify-end items-center mr-4">
+		<a href="/" class="text-white hover:text-gray-300 mr-4">Home</a>
+		<a href="/shop" class="text-white hover:text-gray-300 mr-4">Shop</a>
+		<a href="/driver" class="text-white hover:text-gray-300 mr-4">Driver</a>
+		<a href="/farmer" class="text-white hover:text-gray-300 mr-4">Farmer</a>
+		<a href="/chat" class="text-white hover:text-gray-300 mr-4">Chat</a>
+		<a href="/shoppingCart" class="mr-4"
+			><i class="fa fa-shopping-basket" aria-hidden="true"></i></a
+		>
+		<a href="/profile" class="text-white hover:text-gray-300 mr-4"
+			>{session?.user.email}</a
+		>
+	</nav>
+</div>
+{/if}
 
 <div class="grid place-content-center m-5 text-white">
 	<h1 class="bg-blue-500 rounded-xl p-4 text-2xl cursor-default">Home</h1>
