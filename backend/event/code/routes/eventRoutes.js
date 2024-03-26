50256944bc97d2e47cd5c648e72406c9bf6abc67
sowavy <erom0001@hz.nl>
2024-03-26 13:29:58 +0100

    Finished Join Event

--- backend/event/code/routes/eventRoutes.js
+++ backend/event/code/routes/eventRoutes.js
@@ -6,7 +6,9 @@ const router = express.Router();
 // Route to get all event.
 router.get('/api/events/', eventController.getAllEventData);
 router.post('/api/events/', eventController.createEvent);
+router.get('/api/event_registration/', eventController.getAllRegistrationData);
 router.delete('/api/events/:id', eventController.deleteEvent);
-
+router.post('/api/event_registration/', eventController.joinEvent);
+router.delete('/api/event_registration/:id', eventController.leaveEvent);
   
 export default router;
